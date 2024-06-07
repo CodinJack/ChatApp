cb0afe4efdbfbbce90674f99a4b689c5170eac0d
CodinJack <IIT2022005@iiita.ac.in>
2024-06-07 14:33:28 +0300

    frontend updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Login from './Components/Login';
-import Chat from './Components/Chat';
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./Context/AuthContext";
 
-const App = () => {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Login} />
-        <Route path="/chat" component={Chat} />
-      </Switch>
-    </Router>
-  );
-};
+function App() {
+	const { authUser } = useAuthContext();
+	return (
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
+}
 
 export default App;
--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,72 +1,64 @@
-import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import useLogin from "../../hooks/useLogin";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const navigate = useNavigate();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:3000/api/auth/login', {
-        username,
-        password,
-      });
-      localStorage.setItem('token', response.data.token);
-      navigate('/chat');
-    } catch (err) {
-      setError('Login failed. Please try again.');
-    }
-  };
+	const { loading, login } = useLogin();
 
-  return (
-    <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-        <h1 className='text-3xl font-semibold text-center text-gray-300'>
-          Login
-          <span className='text-blue-500'> ChatApp</span>
-        </h1>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label className='label p-2'>
-              <span className='text-base label-text'>Username</span>
-            </label>
-            <input
-              type='text'
-              placeholder='Enter username'
-              className='w-full input input-bordered h-10'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div>
-            <label className='label'>
-              <span className='text-base label-text'>Password</span>
-            </label>
-            <input
-              type='password'
-              placeholder='Enter Password'
-              className='w-full input input-bordered h-10'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <Link to='/signup' className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
-            {"Don't"} have an account?
-          </Link>
-          {error && <p className='text-red-500 mt-2'>{error}</p>}
-          <div>
-            <button type='submit' className='btn btn-block btn-sm mt-2'>
-              Login
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await login(username, password);
+	};
+
+	return (
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Login
+					<span className='text-blue-500'> ChatApp</span>
+				</h1>
+
+				<form onSubmit={handleSubmit}>
+					<div>
+						<label className='label p-2'>
+							<span className='text-base label-text'>Username</span>
+						</label>
+						<input
+							type='text'
+							placeholder='Enter username'
+							className='w-full input input-bordered h-10'
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
+					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+						{"Don't"} have an account?
+					</Link>
+
+					<div>
+						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
+							{loading ? <span className='loading loading-spinner '></span> : "Login"}
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 };
-
 export default Login;
